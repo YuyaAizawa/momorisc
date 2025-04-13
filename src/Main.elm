01cf423318758b243af510c16f09b25ea2360d4f
YuyaAizawa <yaizawa@sa.cs.titech.ac.jp>
2025-04-13 20:57:48 +0900

    feat: realocate tab UI outside the editor for add the other tabs

--- src/Main.elm
+++ src/Main.elm
@@ -7,6 +7,7 @@ import Html exposing (Html)
 import Html.Attributes as Attr
 import Html.Events exposing (onClick)
 import Html.Events.Extra exposing (onChange)
+import Task
 import Time
 
 import MomoRisc.Cpu as Cpu exposing (Cpu, MemoryAccessPhaseAction(..))
@@ -38,6 +39,7 @@ type alias Model =
   , memory : Memory
   , chario : Chario
   , source : String
+  , defaultInput : String
   , errors : Errors
   , debug : DebugInfo
   , lastCycle : Cpu
@@ -48,8 +50,8 @@ type alias Model =
 
 
 type Tab
-  = EditTab
-  | CheckAndRunTab
+  = EditorTab
+  | RunnerTab
 
 
 init : Model
@@ -63,11 +65,12 @@ init =
     , memory = Memory.zeros
     , chario = Device.charioInit |> Device.setInput sampleInput
     , source = sampleCode
+    , defaultInput = sampleInput
     , errors = errors
     , debug = debug
     , lastCycle = Cpu.init
     , wroteAddr = Nothing
-    , tab = CheckAndRunTab
+    , tab = RunnerTab
     , speed = Stop
     }
 
@@ -102,8 +105,11 @@ sampleInput = "CSIRomoM"
 ------------
 
 type Msg
-  = SourceEdited String
-  | TabClicked Tab
+  = TabChange Tab
+  | SourceEdited String
+  | DefaultInputEdited String
+  | Compile
+  | ResetMachine
   | Step
   | SpeedChange Speed
   | CharioInputEdited String
@@ -124,188 +130,287 @@ type alias MemoryAccessPhaseResult =
   }
 
 
-update : Msg -> Model -> ( Model, Cmd msg )
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  let
-    model_ =
-      case msg of
-        SourceEdited str ->
-          { model | source = str }
+  case msg of
 
-        TabClicked tab ->
-          if tab == model.tab then
-            model
-          else
-            case tab of
-              EditTab ->
-                { model
-                | speed = Stop
-                , tab = tab }
+    TabChange tab ->
+      if tab == model.tab then
+        model |> noCmd
+      else
+        { model
+        | speed = Stop
+        , tab = tab
+        } |> noCmd
 
-              CheckAndRunTab ->
-                let
-                  ( program, debug, errors ) =
-                    Program.compile model.source
-                in
-                  { model
-                  | program = program
-                  , debug = debug
-                  , errors = errors
-                  , cpu = Cpu.init
-                  , memory = Memory.zeros
-                  , lastCycle = Cpu.init
-                  , wroteAddr = Nothing
-                  , tab = tab
-                  }
 
-        Step ->
-          let
-            result =
-              case Cpu.step model.program model.cpu of
-                NoAccessNeeded cpu ->
-                  MemoryAccessPhaseResult cpu model.memory model.chario Nothing
+    SourceEdited str ->
+      { model | source = str }
+        |> noCmd
 
-                ReadNeeded addr fun ->
-                  let
-                    ( data, chario ) =
-                      case Dudit.toInt addr of
-                        90 -> Device.readNumber model.chario
-                        91 -> Device.readChar model.chario
-                      --92 -> plottouchGetX
-                      --93 -> plottouchGetY
-                        x ->
-                          if x < 90 then
-                            ( Memory.read addr model.memory, model.chario )
-                          else
-                            ( Dudit.zero, model.chario )
 
-                    cpu = fun data
-                  in
-                    MemoryAccessPhaseResult cpu model.memory chario Nothing
+    DefaultInputEdited str ->
+      { model | defaultInput = str }
+        |> noCmd
 
-                WriteNeeded addr data cpu ->
-                  let
-                    ( memory, chario ) =
-                      case Dudit.toInt addr of
-                        90 -> ( model.memory, Device.writeNumber data model.chario )
-                        91 -> ( model.memory, Device.writeChar data model.chario )
-                      --92 -> plottouchSetX
-                      --93 -> plottouchSetY
-                      --94 -> plottouchExecute
-                        x ->
-                          if x < 90 then
-                            ( Memory.write addr data model.memory, model.chario )
-                          else
-                            ( model.memory, model.chario )
-                  in
-                    MemoryAccessPhaseResult cpu memory chario (Just addr)
-          in
-            { model
-            | cpu = result.cpu
-            , memory = result.memory
-            , chario = result.chario
-            , lastCycle = model.cpu
-            , wroteAddr = result.wroteAddr
-            }
 
-        SpeedChange speed ->
-          { model | speed = speed }
+    Compile ->
+      let
+        ( program, debug, errors ) =
+          Program.compile model.source
 
-        CharioInputEdited str ->
-          { model | chario = Device.setInput str model.chario }
+        model_ =
+          { model
+          | program = program
+          , debug = debug
+          , errors = errors
+          }
+      in
+        ( model_, continueMsgs [ ResetMachine, TabChange RunnerTab ] )
 
-        CharioOutputClear ->
-          { model | chario = Device.clearOutput model.chario }
 
-  in
-    ( model_, Cmd.none )
+    ResetMachine ->
+      { model
+      | cpu = Cpu.init
+      , memory = Memory.zeros
+      , lastCycle = Cpu.init
+      , wroteAddr = Nothing
+      , chario = Device.charioInit |> Device.setInput model.defaultInput
+      , speed = Stop
+      } |> noCmd
 
+
+    Step ->
+      let
+        result =
+          case Cpu.step model.program model.cpu of
+            NoAccessNeeded cpu ->
+              MemoryAccessPhaseResult cpu model.memory model.chario Nothing
+
+            ReadNeeded addr fun ->
+              let
+                ( data, chario ) =
+                  case Dudit.toInt addr of
+                    90 -> Device.readNumber model.chario
+                    91 -> Device.readChar model.chario
+                  --92 -> plottouchGetX
+                  --93 -> plottouchGetY
+                    x ->
+                      if x < 90 then
+                        ( Memory.read addr model.memory, model.chario )
+                      else
+                        ( Dudit.zero, model.chario )
+
+                cpu = fun data
+              in
+                MemoryAccessPhaseResult cpu model.memory chario Nothing
+
+            WriteNeeded addr data cpu ->
+              let
+                ( memory, chario ) =
+                  case Dudit.toInt addr of
+                    90 -> ( model.memory, Device.writeNumber data model.chario )
+                    91 -> ( model.memory, Device.writeChar data model.chario )
+                  --92 -> plottouchSetX
+                  --93 -> plottouchSetY
+                  --94 -> plottouchExecute
+                    x ->
+                      if x < 90 then
+                        ( Memory.write addr data model.memory, model.chario )
+                      else
+                        ( model.memory, model.chario )
+              in
+                MemoryAccessPhaseResult cpu memory chario (Just addr)
+      in
+        { model
+        | cpu = result.cpu
+        , memory = result.memory
+        , chario = result.chario
+        , lastCycle = model.cpu
+        , wroteAddr = result.wroteAddr
+        }  |> noCmd
+
+    SpeedChange speed ->
+      { model | speed = speed }
+        |> noCmd
+
+    CharioInputEdited str ->
+      { model | chario = Device.setInput str model.chario }
+        |> noCmd
+
+    CharioOutputClear ->
+      { model | chario = Device.clearOutput model.chario }
+        |> noCmd
+
+
+noCmd : Model -> ( Model, Cmd msg )
+noCmd model =
+  ( model, Cmd.none )
+
+
+continueMsgs : List Msg -> Cmd Msg
+continueMsgs =
+  List.map (\msg -> Task.perform (\_ -> msg) (Task.succeed ()))
+    >> Cmd.batch
 
 
 ----------
 -- VIEW --
 ----------
 
+type alias TabBuildInfo =
+  { title: String
+  , labelId: String
+  , panelId: String
+  , contents: Model -> List (Html Msg)
+  }
+
+
+tabBuildInfo : Tab -> TabBuildInfo
+tabBuildInfo tab =
+  case tab of
+    EditorTab ->
+      { title = "Editor"
+      , labelId = "editor-tab"
+      , panelId = "editor-panel"
+      , contents = editorContents
+      }
+
+    RunnerTab ->
+      { title = "Runner"
+      , labelId = "runner-tab"
+      , panelId = "runner-panel"
+      , contents = runnerContents
+      }
+
+
 view : Model -> Html Msg
 view model =
-  Html.div [ Attr.id "elm-area" ]
-    [ Html.div [ Attr.class "flex-container" ]
-      [ Html.div [ Attr.class "program-pane" ]
-          [ Html.h3 [] [ Html.text "Program" ]
-          , programView model
-          ]
-      , Html.div [ Attr.class "data-pane" ]
-        [ Html.h3 [] [ Html.text "Register" ]
-        , Html.div [ Attr.class "registers box-frame" ]
-          [ registerView "PC" model.cpu.pc model.lastCycle.pc
-          , registerView "A" model.cpu.a model.lastCycle.a
-          , registerView "B" model.cpu.b model.lastCycle.b
-          , registerView "C" model.cpu.c model.lastCycle.c
-          , registerView "D" model.cpu.d model.lastCycle.d
-          ]
-        , Html.h3 [] [ Html.text "Memory" ]
-        , memoryView model.wroteAddr model.memory
-        ]
-      ]
-    , charioView model.chario
-    ]
-
-
-brIfEmpty : String -> Html msg
-brIfEmpty str =
-  case str of
-    "" ->
-      Html.br [] []
-
-    _ ->
-      Html.text str
-
-
-
--- PROGRAM VIEW --
-
-programView : Model -> Html Msg
-programView model =
   let
-    tabView tab text =
-      Html.button
-        [ Attr.class "tab"
-        , ariaSelected (model.tab == tab)
-        , onClick (TabClicked tab)
-        ]
-        [ Html.text text ]
+    tabs = [ EditorTab, RunnerTab ]
+
+    ( labels, panels ) =
+      tabs
+        |> List.map (labelAndPanel model)
+        |> List.unzip
   in
-    Html.div [ Attr.class "tab-container" ]
-      [ Html.div [ Attr.class "tab-nav" ]
-        [ Html.div [ Attr.class "tabs" ]
-          [ tabView EditTab "Edit"
-          , tabView CheckAndRunTab "Check & Run"
-          ]
+    Html.div [ Attr.id "elm-area" ]
+      [ Html.div [ Attr.class "tab-container" ]
+        [ Html.div
+          [ Attr.class "tab-labels"
+          , Attr.attribute "role" "tablist"
+          ] labels
+        , Html.div [ Attr.class "tab-content" ]
+          panels
         ]
-      , Html.div [ Attr.class "tab-content" ]
-          ( case model.tab of
-            EditTab ->
-              [ editView model.source ]
-            CheckAndRunTab ->
-              [ checkAndRunView model.source model.debug model.errors model.cpu ]
-          )
       ]
 
 
-editView : String -> Html Msg
-editView source =
-  Html.textarea
-    [ Attr.class "program editor-content box-frame"
-    , Attr.placeholder "Program"
-    , onChange SourceEdited
-    , Attr.value source
-    , Attr.spellcheck False
+labelAndPanel : Model -> Tab -> ( Html Msg, Html Msg )
+labelAndPanel model tab =
+  let
+    { title, labelId, panelId, contents } =
+      tabBuildInfo tab
+
+    ( selected, hidden ) =
+      if model.tab == tab then
+        ( "true", "false" )
+      else
+        ( "false", "true" )
+
+    label =
+      Html.button
+        [ Attr.id labelId
+        , Attr.class "tab"
+        , Attr.attribute "role" "tab"
+        , Attr.attribute "aria-selected" selected
+        , Attr.attribute "aria-controles" panelId
+        , onClick <| TabChange tab
+        ]
+        [ Html.text title ]
+
+    panel =
+      Html.div
+        [ Attr.id panelId
+        , Attr.attribute "role" "tabpanel"
+        , Attr.attribute "aria-hidden" hidden
+        , Attr.attribute "aria-labelledby" labelId
+        ]
+        (contents model)
+  in
+    ( label, panel )
+
+
+
+-- EDITOR CONTENTS --
+
+editorContents : Model -> List (Html Msg)
+editorContents model =
+  [ Html.h3 [] [ Html.text "Program" ]
+  , Html.div [ Attr.class "button-overlay-wrapper" ]
+    [ Html.textarea
+      [ Attr.class "program source-code box-frame"
+      , Attr.placeholder "Program"
+      , onChange SourceEdited
+      , Attr.value model.source
+      , Attr.spellcheck False
+      ] []
+    , Html.div [Attr.class "buttons"]
+      [ Html.button [ onClick Compile ] [ Html.text "Compile" ] ]
     ]
-    []
+  , Html.h3 [] [ Html.text "Default Input"]
+  , Html.input
+    [ Attr.type_ "text"
+    , Attr.class "chario box-frame"
+    , Attr.placeholder "Program default input here..."
+    , Attr.value model.defaultInput
+    , onChange DefaultInputEdited
+    ] []
+  ]
 
 
-checkAndRunView : String -> Dict LineNum Dudit -> Dict LineNum ParseErr -> Cpu -> Html Msg
-checkAndRunView source debug errors cpu =
+
+-- RUNNER CONTENSTS --
+
+runnerContents : Model -> List (Html Msg)
+runnerContents model =
+  [ Html.div [ Attr.class "two-column-wrapper" ]
+      [ Html.div [ Attr.class "left-column" ]
+        [ Html.h3 [] [ Html.text "Program" ]
+        , checkAndRunArea model.source model.debug model.errors model.cpu
+        ]
+      , Html.div [ Attr.class "right-column" ]
+        [ Html.h3 [] [ Html.text "Register" ]
+        , registerArea model.cpu model.lastCycle
+        , Html.h3 [] [ Html.text "Memory" ]
+        , memoryArea model.wroteAddr model.memory
+        ]
+      ]
+  , Html.h3 [] [ Html.text "Input" ]
+  , Html.input
+    [ Attr.type_ "text"
+    , Attr.class "chario box-frame"
+    , Attr.placeholder "Program input here..."
+    , Attr.value (Device.getInput model.chario)
+    , onChange CharioInputEdited
+    ] []
+  , Html.div [ Attr.class "h-block" ]
+    [ Html.h3 [] [ Html.text "Output" ]
+    , Html.button [ onClick CharioOutputClear ]
+      [ Html.text "clear" ]
+    ]
+  , Html.div [ Attr.class "chario box-frame" ]
+    [ Html.pre []
+      [ Html.text <| Device.getOutput model.chario ]
+    ]
+  ]
+
+
+
+-- CHECK AND RUN AREA --
+
+checkAndRunArea : String -> Dict LineNum Dudit -> Dict LineNum ParseErr -> Cpu -> Html Msg
+checkAndRunArea source debug errors cpu =
   let
     addrAndCodeView : LineNum -> String -> ( Html msg, Html msg )
     addrAndCodeView lineNum str =
@@ -329,16 +434,17 @@ checkAndRunView source debug errors cpu =
         |> List.indexedMap addrAndCodeView
         |> List.unzip
   in
-    Html.div [ Attr.class "check-and-run-content-wrapper" ]
+    Html.div [ Attr.class "button-overlay-wrapper" ]
       [
-        Html.div [ Attr.class "program check-and-run-content box-frame" ]
+        Html.div [ Attr.class "program compile-result box-frame" ]
           [ Html.div [ Attr.class "code-with-addr" ]
             [ Html.div [ Attr.class "addr" ] lineNums
             , Html.div [ Attr.class "code" ] code
             ]
           ]
       , Html.div [ Attr.class "buttons" ]
-        [ Html.button [ onClick Step ] [ stepMark ]
+        [ Html.button [ onClick ResetMachine ] [ resetMark ]
+        , Html.button [ onClick Step ] [ stepMark ]
         , Html.button [ onClick <| SpeedChange Stop ] [ stopMark ]
         , Html.button [ onClick <| SpeedChange NormalSpeed ] [ playMark ]
         , Html.button [ onClick <| SpeedChange HighSpeed ] [ fastForwardMark ]
@@ -393,6 +499,16 @@ lineView executing str maybeErr =
     Html.div [ Attr.class "line" ] contents
 
 
+brIfEmpty : String -> Html msg
+brIfEmpty str =
+  case str of
+    "" ->
+      Html.br [] []
+
+    _ ->
+      Html.text str
+
+
 wordErr : Int -> String -> List (Html msg)
 wordErr i str =
   str
@@ -407,6 +523,14 @@ wordErr i str =
 errSpan : String -> Html msg
 errSpan str =
   Html.span [ Attr.class "error" ] [ Html.text str ]
+
+
+resetMark : Html msg
+resetMark =
+  Html.div [ Attr.class "mark" ]
+    [ Html.div [ Attr.class "circle-arrow" ]
+      [ Html.div [ Attr.class "arrowhead" ] [] ]
+    ]
 
 
 stepMark : Html msg
@@ -440,16 +564,20 @@ fastForwardMark =
     ]
 
 
-ariaSelected : Bool -> Html.Attribute msg
-ariaSelected bool =
-    Attr.attribute "aria-selected" (if bool then "true" else "false")
 
+-- REGISTER AREA --
 
+registerArea current last =
+  Html.div [ Attr.class "registers box-frame" ]
+    [ registerItem "PC" current.pc last.pc
+    , registerItem "A" current.a last.a
+    , registerItem "B" current.b last.b
+    , registerItem "C" current.c last.c
+    , registerItem "D" current.d last.d
+    ]
 
--- REGISTER VIEW --
-
-registerView : String -> Dudit -> Dudit -> Html msg
-registerView label value prev =
+registerItem : String -> Dudit -> Dudit -> Html msg
+registerItem label value prev =
   let
     text = Html.text (Dudit.toString value)
 
@@ -466,10 +594,10 @@ registerView label value prev =
 
 
 
--- MEMORY VIEW --
+-- MEMORY AREA --
 
-memoryView : Maybe Dudit -> Memory -> Html msg
-memoryView wroteAddr memory =
+memoryArea : Maybe Dudit -> Memory -> Html msg
+memoryArea wroteAddr memory =
   let
     celler coords =
       case coords of
@@ -530,32 +658,6 @@ table attributes columns rows celler =
         |> Html.tbody []
   in
     Html.table attributes [ tbody ]
-
-
-
--- CHARIO VIEW --
-
-charioView : Chario -> Html Msg
-charioView chario =
-  Html.div []
-    [ Html.h3 [] [ Html.text "Input" ]
-    , Html.input
-      [ Attr.type_ "text"
-      , Attr.class "chario box-frame"
-      , Attr.placeholder "Program input here..."
-      , Attr.value (Device.getInput chario)
-      , onChange CharioInputEdited
-      ] []
-    , Html.div [ Attr.class "h-block" ]
-      [ Html.h3 [] [ Html.text "Output" ]
-      , Html.button [ onClick CharioOutputClear ]
-        [ Html.text "clear" ]
-      ]
-    , Html.div [ Attr.class "chario box-frame" ]
-      [ Html.pre []
-        [ Html.text <| Device.getOutput chario ]
-      ]
-    ]
 
 
 
